62d43605facd409e6f2aca7d3705a98702cc6fa0
Vinod Singh Bist <32848053+vinodbistvb@users.noreply.github.com>
2021-04-18 22:35:21 +0530

    c

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -15,52 +15,58 @@ namespace GradeBook.GradeBooks
         public override char GetLetterGrade(double averageGrade)
         {
 
-            // List<double> classAverageGrades = new List<double>();
-
-
             if (Students.Count < 5)
             {
 
                 throw new InvalidOperationException("Number of Students are less than 5");
             }
-            double[] classAverageGrades = new double[Students.Count];
-
-            // 20% of the class
-            var twentyPercentOfClass = (int)Math.Ceiling(Students.Count * 0.2);
-            var i = 0;
-
-            foreach (var student in Students)
+            else
             {
-                classAverageGrades[i] = student.AverageGrade;
-                i++;
+                double[] classAverageGrades = new double[Students.Count];
+
+                // 20% of the class
+                var twentyPercentOfClass = (int)Math.Ceiling(Students.Count * 0.2);
+                var i = 0;
+
+                foreach (var student in Students)
+                {
+                    classAverageGrades[i] = student.AverageGrade;
+                    i++;
+                }
+
+                // sort the array
+
+                Array.Sort(classAverageGrades);
+                Array.Reverse(classAverageGrades);
+
+                // find index of averageGrade provided in class AverageGrade
+                // Compair with the 20% multiplier to see where it fits. 
+
+                if (averageGrade >= classAverageGrades[twentyPercentOfClass - 1])
+                {
+                    return 'A';
+                }
+                else if (averageGrade >= classAverageGrades[twentyPercentOfClass * 2 - 1])
+                {
+                    return 'B';
+                }
+                else if (averageGrade >= classAverageGrades[twentyPercentOfClass * 3 - 1])
+                {
+                    return 'C';
+                }
+                else if (averageGrade >= classAverageGrades[twentyPercentOfClass * 4 - 1])
+                {
+                    return 'D';
+                }
+                else
+                {
+                    return 'F';
+                }
             }
 
-            // sort the aray
 
-            Array.Sort(classAverageGrades);
-            Array.Reverse(classAverageGrades);
 
-            // find index of averageGrade provided in class AverageGrade
-            // Compair with the 20% multiplier to see where it fits. 
 
-            if (averageGrade >= classAverageGrades[twentyPercentOfClass - 1])
-            {
-                return 'A';
-            }
-            else if (averageGrade >= classAverageGrades[twentyPercentOfClass * 2 - 1])
-            {
-                return 'B';
-            }
-            else if (averageGrade >= classAverageGrades[twentyPercentOfClass * 3 - 1])
-            {
-                return 'C';
-            }
-            else if (averageGrade >= classAverageGrades[twentyPercentOfClass * 4 - 1])
-            {
-                return 'D';
-            }
-
-            return 'F';
         }
     }
 }
