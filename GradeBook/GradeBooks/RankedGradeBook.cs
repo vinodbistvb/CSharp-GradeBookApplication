1191b4a0b837d9b8953f0e3f416be00cbbc1efe4
Vinod Singh Bist <32848053+vinodbistvb@users.noreply.github.com>
2021-04-18 16:16:47 +0530

    Check  My Work

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,14 +1,15 @@
-﻿using System;
+﻿using GradeBook.Enums;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace GradeBook.GradeBooks
 {
-    class RankedGradeBook : BaseGradeBook
+    public class RankedGradeBook : BaseGradeBook
     {
         public RankedGradeBook(string name): base(name)
         {
-            Type = Enums.GradeBookType.Ranked;
+            Type = GradeBookType.Ranked;
         }
     }
 }
--- GradeBook/GradeBooks/StandardGradeBook.cs
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -1,14 +1,15 @@
-﻿using System;
+﻿using GradeBook.Enums;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace GradeBook.GradeBooks
 {
-    class StandardGradeBook : BaseGradeBook
+    public class StandardGradeBook : BaseGradeBook
     {
         public StandardGradeBook(string name):base(name)
         {
-            Type = Enums.GradeBookType.Standard;
+            Type = GradeBookType.Standard;
         }
     }
 }
