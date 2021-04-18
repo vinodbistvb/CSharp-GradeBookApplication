cf11e501a17218fc99b78105b9278cf6fa084f34
Vinod Singh Bist <32848053+vinodbistvb@users.noreply.github.com>
2021-04-18 22:44:14 +0530

    CalculateStatistics
    CalculateStatistics

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -68,5 +68,18 @@ namespace GradeBook.GradeBooks
 
 
         }
+
+        public override void CalculateStatistics()
+        {
+            if(Students.Count >= 5)
+            {
+                base.CalculateStatistics();
+            }
+            else
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+            }
+           
+        }
     }
 }
